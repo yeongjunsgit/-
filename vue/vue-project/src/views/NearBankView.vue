8a0366218d185c20c4673c427b9e4495fc98ce97
unknown <dudwns020202@naver.com>
2023-11-23 01:33:54 +0900

    환율 계산기 구현 완료

--- vue/vue-project/src/views/NearBankView.vue
+++ vue/vue-project/src/views/NearBankView.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <!-- <Map /> -->
+    <h1>지도</h1>
+    <div ref="map" style="width: 100%; height: 400px;"></div>
   </div>
 </template>
 
 <script setup>
-// import Map from '@/components/Map.vue'
+// 브라우저에서 직접 실행할 때는 Vue 컴포넌트를 전역 스코프에 등록
+const MapView = {
+  template: '<div ref="map" style="width: 100%; height: 400px;"></div>',
+  mounted() {
+    const container = this.$refs.map;
+    const options = {
+      center: new kakao.maps.LatLng(37.566826, 126.9786567),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+    // 기타 맵에 필요한 작업 수행...
+  },
+};
 
-// export default {
-//   name: "NearBankView",
-//   components: {
-//     Map,
-//   },
-
-
+// 등록된 컴포넌트를 전역 스코프에 할당
+window.MapView = MapView;
 </script>
 
 <style scoped>
